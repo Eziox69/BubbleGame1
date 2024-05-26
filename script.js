98a66876252c503ba6f90abff5a17b61b2b7d030
Harivansh <harivanshwork@gmail.com>
2024-05-27 01:37:25 +0530

    br

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function runtimer(){
      }else{
          clearInterval(timerint);
          
-         document.querySelector("#pbtm").innerHTML=`<h1>Game Over</h1>  <h2>Your Score is ${score}</h2>`
+         document.querySelector("#pbtm").innerHTML=`<h1>Game Over</h1> <br><h2>Your Score is ${score}</h2>`
          document.querySelector("#hitval").innerHTML="-";
      }
     },1000)
